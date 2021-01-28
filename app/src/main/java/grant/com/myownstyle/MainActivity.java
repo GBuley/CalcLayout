9ddc1a0ffc5a6cb4bb1e52be309420c7d02edf45
BuleyG <gtbuley@gmail.com>
2021-01-27 23:17:42 -0600

    Literally everything... and it looks bad :/

---
+++ app/src/main/java/grant/com/myownstyle/MainActivity.java
@@ -0,0 +1,19 @@
+package grant.com.myownstyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.calculator_main);
+    }
+}
