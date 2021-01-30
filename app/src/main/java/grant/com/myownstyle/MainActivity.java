506ec1b351a900d9a40f1af1c06a883e08717b58
BuleyG <gtbuley@gmail.com>
2021-01-30 01:48:47 -0600

    I can't believe this actually works'

--- app/src/main/java/grant/com/myownstyle/MainActivity.java
+++ app/src/main/java/grant/com/myownstyle/MainActivity.java
@@ -4,19 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
     private MaterialTextView equation;
     private MaterialTextView calculation;
+    private MaterialTextView resultText;
     private String strButton= "";
-    private char[] nums ={0,1,2,3,4,5,6,7,8,9};
-    private char[] operations ={'+','-','/','x'};
+    private final char[] operations ={'+','-','/','x'};
+    private final ArrayList<String> stringNums = new ArrayList<>();
+    private final ArrayList<Double> nums = new ArrayList<>();
+    Queue<Character> numQueue = new LinkedList<>();
+    LinkedList<Character> operationQueue = new LinkedList<>();
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){
+        }
+        setContentView(R.layout.calculator_main);
+
+        int[] ids = {R.id.percent,R.id.close_paren,R.id.open_paren,R.id.one,R.id.two,R.id.three,R.id.four,R.id.five,R.id.six,R.id.seven,R.id.eight,R.id.nine,R.id.zero,R.id.period,R.id.div,R.id.side_plus,R.id.minus, R.id.x};
+        for(int id:ids){
+            buttonPressed(id);
+        }
+        clearButtonPressed();
+        equalButtonPressed(R.id.equal);
+
+    }
+
+    private void clearButtonPressed() {
+        MaterialButton button = findViewById(R.id.clear);
+        equation = findViewById(R.id.equation_text);
+        resultText = findViewById(R.id.result_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText("");
+                equation.setText("");
+                strButton = "";
+            }
+        });
+    }
 
     public void buttonPressed(int button_id){
         MaterialButton button = findViewById(button_id);
@@ -39,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     String result = String.valueOf(equation.getText());
-                    double actualValue = parseValue(result);
-                    calculation.setText("= "+ result);
+                    double actualValue = getResult(result);
+                    calculation.setText("= "+ actualValue);
                 }
             });
         }
@@ -49,48 +90,87 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private double parseValue(String result) {
+    private double getResult(String result) {
         if(result.equals("")){
             return 0;
         }
         else {
             char[] resultChars = result.toCharArray();
-            int resultCharsLength = resultChars.length;
-            for(char o:operations){
-                if(resultChars[0]==o){
-                    return 0;
-                }
-                if(resultChars[resultCharsLength] == o){
-                    return 0;
-                }
+            if(checkIfInvalidOperationUsed(resultChars)){
+                return 0;
             }
+
             for(char c:resultChars){
-                switch (c){
-                    case 1:
-
+                if(Character.isDigit(c)){
+                    numQueue.add(c);
+                }
+                else{
+                    listNumber(numQueue);
+                    operationQueue.add(c);
                 }
             }
+            listNumber(numQueue);
+            return equationBuilder();
         }
-            return 0;
-
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try
-        {
-            this.getSupportActionBar().hide();
-        }
-        catch (NullPointerException e){}
-        setContentView(R.layout.calculator_main);
-
-        int[] ids = {R.id.percent,R.id.close_paren,R.id.open_paren,R.id.one,R.id.two,R.id.three,R.id.four,R.id.five,R.id.six,R.id.seven,R.id.eight,R.id.nine,R.id.zero,R.id.plus,R.id.period,R.id.div,R.id.side_plus,R.id.minus};
-        for(int id:ids){
-            buttonPressed(id);
+    private double equationBuilder() {
+        if(operationQueue.isEmpty()){
+            listNumber(numQueue);
+            return Double.parseDouble(stringNums.get(0));
         }
 
-        equalButtonPressed(R.id.equal);
+        double finalNumber;
+        finalNumber = 0;
+        for(int i=0; i<operationQueue.size();i++){
+            if(i==0){
+                if(operationQueue.get(0)=='+'){
+                    finalNumber = Double.parseDouble(stringNums.get(i))+Double.parseDouble(stringNums.get(i+1));
+                }if(operationQueue.get(0)=='/'){
+                    finalNumber = Double.parseDouble(stringNums.get(i))/Double.parseDouble(stringNums.get(i+1));
+                }if(operationQueue.get(0)=='X'){
+                    finalNumber = Double.parseDouble(stringNums.get(i))*Double.parseDouble(stringNums.get(i+1));
+                }if(operationQueue.get(0)=='-'){
+                    finalNumber = Double.parseDouble(stringNums.get(i))-Double.parseDouble(stringNums.get(i+1));
+                }
+            }
+            else{
+                if(operationQueue.get(i)=='+'){
+                    finalNumber += Double.parseDouble(stringNums.get(i+1));
+                }if(operationQueue.get(i)=='/'){
+                    finalNumber /= Double.parseDouble(stringNums.get(i+1));
+                }if(operationQueue.get(i)=='X'){
+                    finalNumber *= Double.parseDouble(stringNums.get(i+1));
+                }if(operationQueue.get(i)=='-'){
+                    finalNumber -= Double.parseDouble(stringNums.get(i+1));
+                }
+            }
+        }
+        stringNums.clear();
+        operationQueue.clear();
+        return finalNumber;
+    }
 
+
+    private void listNumber(Queue<Character> numQueue) {
+        StringBuilder sb = new StringBuilder();
+       for(char c:numQueue){
+           sb.append(c);
+       }
+       stringNums.add(sb.toString());
+       numQueue.clear();
+    }
+
+    private boolean checkIfInvalidOperationUsed(char[] resultChars) {
+        int resultCharsLength = resultChars.length;
+        for(char o:operations){
+            if(resultChars[0]==o){
+                return true;
+            }
+            if(resultChars[resultCharsLength-1] == o){
+                return true;
+            }
+        }
+        return false;
     }
 }
